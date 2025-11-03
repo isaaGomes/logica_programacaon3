b7c9a1c43cd9ca2a898b9a1c466934a43e4444e4
ISABELA GOMES DA SILVA <isabela.gsilva5@senacsp.edu.br>
2025-11-03 20:42:21 -0300

    primeiro commit com codigo

---
+++ AULA 1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//f5- EXECUTAR EM MODO DEBUG
+//CTRL K+C- COMENTA LINHA
+//CTRL K+U - DESCOMENTA LINHA
+//CTRL +SHIFT +  B = COMPILAR 
+
+// declaração de uma variavel
+string nome;
+
+//atribuicao de valor a uma variavel
+nome = "isabela gomes";
+
+Console.WriteLine(nome);
+
+nome = "joão";
+
+Console.WriteLine(nome);
+
+//DECLARACAO = ATRIBUICAO DE UMA CONSTANTE
+const string constante = "minha constante";
+
+Console.WriteLine(constante);
+
+//tipo nome = valor;
+int idade = 23;
+long inteirogrande = 0;
+float alturafloat = 1.7f;
+double altura = 1.8;
+char letra = 'A';
+
+bool boleano = true;
---
+++ Aula 2 - Operadores/Program.cs
@@ -0,0 +1,20 @@
+﻿//peça para o usuário inserir dois números inteiros e mostre=os no console.
+int n1;
+int n2;
+
+
+Console.WriteLine("informe um numero inteiro:");
+n1 = int.Parse(Console.readline());
+
+
+Console.WriteLine("informe outro numero inteiro:");
+
+
+
+
+
+
+
+
+
+
